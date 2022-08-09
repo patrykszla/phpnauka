5ba6b4ac707861808a3f18b9f325380e943ac3ff
Patryk Szlachta <patryk.szla@interia.pl>
2022-08-09 22:00:22 +0200

    page 77

--- index.php
+++ index.php
@@ -8,6 +8,34 @@ require_once __DIR__."/libs/vendor/autoload.php";
 $smarty = new Smarty;
 $mail = new PHPMailer(true);
 
-if($smarty && $mail){
-    print "zainstalowane";
-}
+// if($smarty && $mail){
+//     print "zainstalowane";
+// }
+
+
+
+// print "lubie programować!";
+
+// $myText = 'PHP';
+
+// print 'ten tekst ma wiele linii '. $myText . ' ';
+// echo "przykład umieszczenia \"cudzyslowiu\" w tekscie";
+
+// $name = "  Robert  ";
+// print "Tekst oryginalny: >" . $name . '<'.PHP_EOL;
+// print "Tekst oryginalny: >" . trim($name) . '<'.PHP_EOL;
+
+// $inwokacja = <<< EOF
+// Litwo ojczyzno moja jestest jak zdrowie cos tam cos 
+// ten tylko sie dowie kto cie stracil gosciu nie gledz 
+// mi tutaj juz koncz wasc wstydu oszczędź.
+// EOF;
+
+// print $inwokacja;
+
+
+
+$tekst = 'Adam,Ewa,Janusz';
+$elementy = explode(',', $tekst);
+print $tekst.PHP_EOL;
+print_r($elementy);
