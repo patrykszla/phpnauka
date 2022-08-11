114d949e6332bd304554f17e986a1d8dcd590478
Patryk Szlachta <patryk.szla@interia.pl>
2022-08-11 21:48:03 +0200

    lesson 9 and 10

---
+++ lesson09.php
@@ -0,0 +1,15 @@
+<?php 
+$a = 25;
+$b = 30;
+
+// if ($a > $b)
+// {
+//     print "zmienna a jest wieksza od zmiennej b";
+// } else 
+// {
+//     print "zmienna a jest mniejsza od zmiennej b";
+// }
+
+$wiek = 12;
+$status = ($wiek >= 18) ? 'jest': 'nie jest';
+print "mateusz ".  $status . " pełnoletni";
