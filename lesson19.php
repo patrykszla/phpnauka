bd542f752ceee63adee7c74dc973dbe34c84750d
Patryk Szlachta <patryk.szla@interia.pl>
2022-09-05 20:52:34 +0200

    range of variables

--- lesson19.php
+++ lesson19.php
@@ -12,3 +12,24 @@ echo "<br />";
 printName();
 echo "<br />";
 print "zmienna globalna $name";
+
+
+echo "<br />";
+
+
+$name2 = 'name numer 2';
+
+function printName2()
+{
+    global $name2;
+    print "globalna przed zmiana: $name2";
+    $name2 = "imie po zmianie globalnej";
+    print "globalna po zmianie: $name2";
+}
+
+print "zmienna globalna: $name2";
+printName2();
+echo "<br />";
+print "zmienna globalna: $name2";
+echo "<br />";
+print_r($_SERVER);
