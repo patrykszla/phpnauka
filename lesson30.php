3ed1c581226bbe4a9ddde24684efd372eddcf9b3
Patryk Szlachta <patryk.szla@interia.pl>
2022-10-05 21:22:19 +0200

    working with class, database

--- lesson30.php
+++ lesson30.php
@@ -31,3 +31,71 @@ class Radio extends Stereo
 
 $radio = new Radio();
 $radio->print3();
+
+
+interface Data
+{
+    public function getID();
+    public function getName();
+    public function getCode();
+}
+
+class Patient implements Data
+{
+    public function getID()
+    {
+        echo "<br />";
+        print "obsluga metody getID();";
+        echo "<br />";
+    }
+
+    public function getName()
+    {
+        print "obluga metody getName()";
+        echo "<br />";
+    }
+
+    public function getCode()
+    {
+        print "oblsuga metody getCode()";
+        echo "<br />";
+    }
+}
+
+$chory = new Patient;
+$chory->getID();
+$chory->getName();
+$chory->getCode();
+
+abstract class Info 
+{
+    abstract public function getID();
+
+    public function getName()
+    {
+        echo "<br />";
+        print "Obsługa metody getName() klasy abstrakcyjnej info";
+    }
+
+    final public function getCode()
+    {
+        echo "<br />";
+        print "Obsluga metody getCode()";
+    }
+}
+
+class Pacjent extends Info
+{
+    public function getID()
+    {
+        print "obsluga metody getId()";
+    }
+
+    
+}
+
+$nfz = new Pacjent();
+$nfz->getID();
+$nfz->getName();
+$nfz->getCode();
+
